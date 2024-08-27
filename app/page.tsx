1a48dad58e231a1d78c7084d0e4ac6cc45e221d6
Jacob Cox <jacobcox@Jacobs-MacBook-Pro.local>
2024-08-26 19:52:15 -0700

    forked weather app

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function App(): JSX.Element {
+  return (
+    <main className={styles.main}>
+      <h1>Weather App</h1>
+    </main>
+  );
+}
