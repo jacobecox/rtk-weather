151749157df7d2d05dbd8544ad3b6ca626082c27
Jacob Cox <jacobcox@Jacobs-MacBook-Pro.local>
2024-08-28 15:57:31 -0700

    added comments in searchBar component

--- app/components/searchBar.tsx
+++ app/components/searchBar.tsx
@@ -4,14 +4,16 @@ interface SearchBarProps {
   placeholder: string;
   onSearch: (query: string) => void;
 }
-
+//onSearch will be passed onto another component that fetches the API request
 const SearchBar = ({ placeholder, onSearch }: SearchBarProps): JSX.Element => {
   const [query, setQuery] = useState<string>("");
 
+  //sets the query to what is being searched
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setQuery(e.target.value);
   };
 
+  //prevents running default requests when submitted
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
   };
